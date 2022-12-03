4bc4ef988fec712ed838776364066bda2b86bd09
Michael <lemichael1215@gmail.com>
2022-12-03 23:10:33 +0300

    add reusable api client and refactor index page

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,15 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  // axios.get("/api/users/currentuser");
 
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // we are on the server
-    // requests should be made to
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of " "
-    const response = await axios.get("/api/users/currentuser");
-    return response.data;
-  }
-
-  return response.data;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
